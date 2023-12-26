93fe29f8d37337d0aa965830db3eaaf8b330ed99
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-12-27 00:17:50 +0100

    chore: Add sequential chains

--- notebook/Projects/Pycode-generation/Simple-code-gen/main.py
+++ notebook/Projects/Pycode-generation/Simple-code-gen/main.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional
+from typing import Any, Optional
 
 import click
 from dotenv import find_dotenv, load_dotenv
@@ -17,11 +17,43 @@ def load_credentials() -> Optional[str]:
     return OPENAI_API_KEY
 
 
-@typechecked
-@click.command()
+@click.group()
 @click.option("--language", help="Name of programming language", default="python")
 @click.option("--task", help="The task you want to perform", default="generate a list of numbers")
-def code_gen(language: str, task: str) -> str:
+@click.pass_context
+def cli(ctx, language: str, task: str) -> None:
+    """Click command object."""
+    ctx.ensure_object(dict)
+    ctx.obj["LANGUAGE"] = language
+    ctx.obj["TASK"] = task
+
+
+# @typechecked
+# @cli.command()
+# @click.option("--language", help="Name of programming language", default="python")
+# @click.option("--task", help="The task you want to perform", default="generate a list of numbers")
+# def code_gen(language: str, task: str) -> str:
+#     """This function is used to create a code generation program using Langchain and an LLM."""
+#     OPENAI_API_KEY: str = load_credentials()
+#     # Create LLM
+#     llm: OpenAI = OpenAI(openai_api_key=OPENAI_API_KEY)
+
+#     # Used for text completion LLMs
+#     prompt_template: str = """Write a very short {language} function that will {task}"""
+#     code_prompt: PromptTemplate = PromptTemplate(
+#         input_variables=["language", "task"], template=prompt_template
+#     )
+#     code_chain: LLMChain = LLMChain(llm=llm, prompt=code_prompt)
+
+#     result = code_chain(inputs={"language": language, "task": task}).get("text")
+#     click.secho(message=result, fg="green")
+#     return result
+
+
+@typechecked
+@cli.command()
+@click.pass_context
+def code_gen(ctx: Any) -> str:
     """This function is used to create a code generation program using Langchain and an LLM."""
     OPENAI_API_KEY: str = load_credentials()
     # Create LLM
@@ -34,10 +66,12 @@ def code_gen(language: str, task: str) -> str:
     )
     code_chain: LLMChain = LLMChain(llm=llm, prompt=code_prompt)
 
-    result = code_chain(inputs={"language": language, "task": task}).get("text")
+    result = code_chain(
+        inputs={"language": ctx.obj.get("LANGUAGE"), "task": ctx.obj.get("TASK")}
+    ).get("text")
     click.secho(message=result, fg="green")
     return result
 
 
 if __name__ == "__main__":
-    code_gen()
+    cli(obj={})
