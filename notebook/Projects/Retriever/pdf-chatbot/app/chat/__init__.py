08b6e01ab9d484379ff10a1c0e3b526c128fddb9
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2024-02-25 20:07:43 +0100

    chore: Code refactor [skip ci]

--- notebook/Projects/Retriever/pdf-chatbot/app/chat/__init__.py
+++ notebook/Projects/Retriever/pdf-chatbot/app/chat/__init__.py
@@ -2,3 +2,11 @@ from .create_embeddings import create_embeddings_for_pdf
 from .score import score_conversation, get_scores
 from .chat import build_chat
 from .models import ChatArgs
+
+__all__: list[str] = [
+    "create_embeddings_for_pdf",
+    "score_conversation",
+    "get_scores",
+    "build_chat",
+    "ChatArgs",
+]
--- notebook/Projects/Retriever/pdf-chatbot/app/web/db/models/__init__.py
+++ notebook/Projects/Retriever/pdf-chatbot/app/web/db/models/__init__.py
@@ -3,3 +3,12 @@ from .pdf import Pdf
 from .conversation import Conversation
 from .message import Message
 from .base import BaseModel as Model
+
+
+__all__: list[str] = [
+    "User",
+    "Pdf",
+    "Conversation",
+    "Message",
+    "Model",
+]
